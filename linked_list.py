508ab7abb2c386e39ed047736bac0b3319d5ba6b
kebbi <kebbi@ualberta.ca>
2018-05-13 23:23:37 -0600

    created doubly linked list class

--- linked_list.py
+++ linked_list.py
@@ -1,7 +1,8 @@
 class Node:
-	def __init__(self, data = None):
+	def __init__(self, data = None, next = None, prev = None):
 		self.data = data
-		self.next = None
+		self.next = next
+		self.prev = prev
 	def __str__(self):
 		return str(data)
 
@@ -56,3 +57,60 @@ class SinglyLinkedList:
 	def clear(self):
 		self.tail = None
 		self.head = None
+
+class DoublyLinkedList(object):
+	def __init__(self):
+		self.head = None
+		self.tail = None
+		self.index = 0
+	
+	def traverse(self):
+		current = self.tail
+		while current:
+			val = current.data
+			current = current.next
+			yield val
+	
+	def append(self, data):
+		new_node = Node(data, None, None)
+		if self.head is None:
+			self.head = new_node
+			self.tail = self.head
+		else:
+			new_node.prev = self.tail
+			self.tail.next = new_node
+			self.tail = new_node
+
+			self.index +=1
+
+	def delete(self, data):
+		current = self.head
+		node_deleted = False
+
+		if current is None:
+			node_deleted = False
+
+		elif current.data == data:
+			self.head = current.next
+			self.head.prev = None
+			node_deleted = True
+
+		elif self.tail.data == data:
+			self.tail = self.tail = prev
+			self.tail.next = None
+			node_deleted = True
+		else:
+			while current:
+				if current.data == data:
+					current.prev.next = current.next
+					current.next.prev = current.prev
+					node_deleted = True
+					current = current.next
+		if node_deleted:
+			self.index -= 1
+
+	def find(self, data):
+		for node_data in self.iter():
+			if data == node_data:
+				return True
+		return false
